ba182d2b6dbcf0d09213617bc0b4d4ac1ee41bf9
koylyakandriy <koylyakandriy@gmail.com>
2023-10-15 14:28:48 +0300

    init app

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+const Page = () => {
+    return (
+        <Text>
+            Page
+        </Text>
+    );
+};
+
+export default Page;
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -16,4 +16,9 @@ export default {
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
   },
+    primary: '#20E1B2',
+    lightGray: '#FCF8FF',
+    gray: '#EEE9F0',
+    medium: '#9F9AA1',
+    green: '#437919',
 };
